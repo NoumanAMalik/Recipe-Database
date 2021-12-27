f6889dd685a1702f205fc34fe420821862a9246f
Nouman <noumancodes@gmail.com>
2021-12-27 15:11:59 -0500

    Edited: Added local storage functionality

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const meals = document.getElementById("meals");
 
 getRandomMeal();
+fetchFavoriteMeals();
 
 async function getRandomMeal() {
     const resp = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
     const respData = await resp.json();
     const randomMeal = respData.meals[0];
 
-    console.log(randomMeal);
-
     addMeal(randomMeal, true);
 }
 
 async function getMealById(id) {
-    const meal = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
+    const resp = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
+
+    const respData = await resp.json();
+    const meal = respData.meals[0];
+
+    return meal;    
 }
 
 async function getMealBySearch(term) {
@@ -33,10 +37,51 @@ function addMeal(mealData, random = false) {
         <button class="fav-button"><i class="fas fa-heart"></i></button>
     </div>
     `;
-
-    meal.querySelector(".meal-body .fav-button").addEventListener("click", () => {
-        alert("hello");
+    const btn = meal.querySelector(".meal-body .fav-button");
+    
+    btn.addEventListener("click", () => {
+        if (btn.classList.contains("active")) {
+            removeMealFromLocalStorage(mealData.idMeal);
+            btn.classList.remove("active");
+        } else {
+            addMealToLocalStorage(mealData.idMeal);
+            btn.classList.toggle("active");
+        }
     });
 
     meals.appendChild(meal);
+}
+
+function addMealToLocalStorage(mealId) {
+    const mealIds = getMealsFromLocalStorage();
+
+    localStorage.setItem("mealIds", JSON.stringify([...mealIds, mealId]));
+}
+
+function removeMealFromLocalStorage(mealId) {
+    const mealIds = getMealsFromLocalStorage();
+
+    localStorage.setItem("mealIds", JSON.stringify(mealIds.filter(id => id !== mealId)));
+}
+
+function getMealsFromLocalStorage() {
+    const mealIds = JSON.parse(localStorage.getItem("mealIds"));
+
+    return mealIds === null ? [] : mealIds;
+}
+
+async function fetchFavoriteMeals() {
+    const mealIds = getMealsFromLocalStorage();
+
+    const meals = [];
+
+    for (let i = 0; i < mealIds.length; i ++) {
+        const mealId = mealIds[i];
+        meal = await getMealById(mealId);
+        meals.push(meal);
+    }
+
+    console.log(meals);
+
+    //TODO: Add them to the screen
 }
